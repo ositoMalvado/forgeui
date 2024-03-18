2b8345b7c405e62cffa5517ed3629d6f97e4c285
Julián Perez <mordecaaii@gmail.com>
2024-03-18 09:20:04 -0300

    forgeui_test

---
+++ terminal.sh
@@ -0,0 +1,13 @@
+﻿cd /home/studio-lab-user
+KERNEL_NAME="Forge_UI"
+PYTHON="3.10.6"
+conda create --yes --name "$KERNEL_NAME" python="$PYTHON"
+conda activate "$KERNEL_NAME"
+pip install --quiet ipykernel
+git config --global user.name "Julian Perez"
+git config --global user.email "mordecaaii@gmail.com"
+git clone https://github.com/lllyasviel/stable-diffusion-webui-forge /home/studio-lab-user/stable-diffusion-webui
+cd stable-diffusion-webui
+pip install -r requirements.txt
+pip install xformers
+conda install gstreamer --yes
